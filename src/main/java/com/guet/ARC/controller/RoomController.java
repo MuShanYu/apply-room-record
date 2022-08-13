6236be8c6e18e9057b632546c9b049cf19d46b90
lidzknyy <2275129519@qq.com>
2022-08-13 13:30:13 +0800

    1、sa-token过滤swagger 2、添加房间模块增删改接口

--- src/main/java/com/guet/ARC/controller/RoomController.java
+++ src/main/java/com/guet/ARC/controller/RoomController.java
@@ -1,9 +1,40 @@
 package com.guet.ARC.controller;
 
 import com.guet.ARC.common.anno.ResponseBodyResult;
-import org.springframework.web.bind.annotation.RestController;
+import com.guet.ARC.domain.Room;
+import com.guet.ARC.service.RoomService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @ResponseBodyResult
+@Api(tags = "房间模块")
+@Validated
 public class RoomController {
+    @Autowired
+    private RoomService roomService;
+
+    // 增加房间
+    @PostMapping("/room/add")
+    @ApiOperation(value = "新增房间")
+    public Room addRoom(@RequestBody Room room) {
+        return roomService.addRoom(room);
+    }
+
+    // 删除房间
+    @GetMapping("/room/delete")
+    @ApiOperation(value = "删除房间")
+    public Room deleteRoom(@RequestParam("id") String id) {
+        return roomService.deleteRoom(id);
+    }
+
+    // 修改房间
+    @PostMapping("/room/update")
+    @ApiOperation(value = "修改房间")
+    public Room updateRoom(@RequestBody Room room) {
+        return roomService.updateRoom(room);
+    }
 }
--- src/main/java/com/guet/ARC/service/RoomService.java
+++ src/main/java/com/guet/ARC/service/RoomService.java
@@ -1,7 +1,58 @@
 package com.guet.ARC.service;
 
+import com.guet.ARC.common.constant.CommonConstant;
+import com.guet.ARC.common.domain.ResultCode;
+import com.guet.ARC.common.exception.AlertException;
+import com.guet.ARC.domain.Room;
+import com.guet.ARC.mapper.RoomMapper;
+import com.guet.ARC.util.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class RoomService {
+    @Autowired
+    private RoomMapper roomMapper;
+
+    @Transactional(rollbackFor = Throwable.class)
+    public Room addRoom(Room room) {
+        long now = System.currentTimeMillis();
+        String id = CommonUtils.generateUUID();
+        room.setId(id);
+        room.setState(CommonConstant.STATE_ACTIVE);
+        room.setCreateTime(now);
+        room.setUpdateTime(now);
+        if (roomMapper.insert(room) == 0) {
+            throw new AlertException(ResultCode.INSERT_ERROR);
+        }
+        return room;
+    }
+
+    @Transactional(rollbackFor = Throwable.class)
+    public Room deleteRoom(String id) {
+        Optional<Room> optionalRoom = roomMapper.selectByPrimaryKey(id);
+        Room room = null;
+        if(optionalRoom.isPresent()) {
+            room = optionalRoom.get();
+        }
+        if (roomMapper.deleteByPrimaryKey(id) == 0) {
+            throw new AlertException(ResultCode.DELETE_ERROR);
+        }
+        return room;
+    }
+
+    @Transactional(rollbackFor = Throwable.class)
+    public Room updateRoom(Room room) {
+        if (room.getId() == null || room.getId().trim().equals("")) {
+            throw new AlertException(ResultCode.PARAM_IS_BLANK);
+        }
+        room.setUpdateTime(System.currentTimeMillis());
+        if (roomMapper.updateByPrimaryKeySelective(room) == 0) {
+            throw new AlertException(ResultCode.UPDATE_ERROR);
+        }
+        return room;
+    }
 }
