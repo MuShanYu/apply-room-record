f7c6a7c586b7bb9f8a2f66862e15ad68e98cc972
yulinfeng <2590213081@qq.com>
2022-08-14 19:09:48 +0800

    修复跨域问题

--- src/main/java/com/guet/ARC/config/SaTokenConfig.java
+++ src/main/java/com/guet/ARC/config/SaTokenConfig.java
@@ -1,7 +1,13 @@
 package com.guet.ARC.config;
 
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.filter.SaServletFilter;
 import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
 import cn.dev33.satoken.interceptor.SaRouteInterceptor;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
+import com.guet.ARC.common.domain.Result;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,11 +29,6 @@ public class SaTokenConfig implements WebMvcConfigurer {
         loginExcludePathPatterns.add("/admin/login");
         loginExcludePathPatterns.add("/user/update/pwd");
         loginExcludePathPatterns.add("/user/get/verifyCode");
-        loginExcludePathPatterns.add("/v3/**");
-        loginExcludePathPatterns.add("/swagger-ui/**");
-        loginExcludePathPatterns.add("/swagger-resources/**");
-        loginExcludePathPatterns.add("/doc.html");
-        loginExcludePathPatterns.add("/error");
 
         // 注解权限拦截!!!!!
         registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**");
@@ -35,5 +36,43 @@ public class SaTokenConfig implements WebMvcConfigurer {
         registry.addInterceptor(new SaRouteInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns(loginExcludePathPatterns);
+    }
+
+    /**
+     * 解决权限与路由过滤，跨域问题
+     * @return
+     */
+    @Bean
+    public SaServletFilter getSaServletFilter() {
+        return new SaServletFilter()
+                // 拦截与排除 path
+                .addInclude("/**").addExclude("/favicon.ico", "/v3/**",
+                        "/swagger-ui/**", "/swagger-resources/**", "/doc.html", "/error")
+
+                // 全局认证函数
+                .setAuth(obj -> {
+                    // 暂时不做处理
+                })
+                // 异常处理函数
+                .setError(e -> {
+                    return Result.fail("路由拦截异常");
+                })
+                // 前置函数：在每次认证函数之前执行
+                .setBeforeAuth(obj -> {
+                    // ---------- 设置跨域响应头 ----------
+                    SaHolder.getResponse()
+                            // 允许指定域访问跨域资源
+                            .setHeader("Access-Control-Allow-Origin", "*")
+                            // 允许所有请求方式
+                            .setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+                            // 有效时间
+                            .setHeader("Access-Control-Max-Age", "3600")
+                            // 允许的header参数
+                            .setHeader("Access-Control-Allow-Headers", "*");
+                    // 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS)
+                            .free(r -> System.out.println("OPTIONS预检请求，不做处理"))
+                            .back();
+                });
     }
 }
