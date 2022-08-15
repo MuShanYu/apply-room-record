de70f54e5c80147a337dbc8d4aced32ebc1dd38f
yulinfeng <2590213081@qq.com>
2022-08-15 14:09:15 +0800

    添加分类数据查询方法

--- src/main/java/com/guet/ARC/controller/DataStatisticsController.java
+++ src/main/java/com/guet/ARC/controller/DataStatisticsController.java
@@ -1,9 +1,35 @@
 package com.guet.ARC.controller;
 
+import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.annotation.SaMode;
 import com.guet.ARC.common.anno.ResponseBodyResult;
+import com.guet.ARC.common.constant.CommonConstant;
+import com.guet.ARC.service.DataStatisticsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Api(tags = "数据统计模块")
 @RestController
 @ResponseBodyResult
 public class DataStatisticsController {
+    @Autowired
+    private DataStatisticsService dataStatisticsService;
+
+    @GetMapping("/user/get/classify/room")
+    @ApiOperation(value = "获取分类信息")
+    public Map<String, Object> getClassifyInfoApi() {
+        return dataStatisticsService.queryClassifyInfo();
+    }
+
+    @GetMapping("/admin/get/classify/institute")
+    @ApiOperation(value = "获取学院分类信息")
+    @SaCheckRole(value = {CommonConstant.ADMIN_ROLE, CommonConstant.SUPER_ADMIN_ROLE}, mode = SaMode.OR)
+    public Map<String, Object> getInstituteApi() {
+        return dataStatisticsService.queryUserInstitute();
+    }
 }
--- src/main/java/com/guet/ARC/service/DataStatisticsService.java
+++ src/main/java/com/guet/ARC/service/DataStatisticsService.java
@@ -1,7 +1,67 @@
 package com.guet.ARC.service;
 
+import com.guet.ARC.mapper.RoomDynamicSqlSupport;
+import com.guet.ARC.mapper.RoomMapper;
+import com.guet.ARC.mapper.UserDynamicSqlSupport;
+import com.guet.ARC.mapper.UserMapper;
+import org.mybatis.dynamic.sql.render.RenderingStrategies;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 @Service
 public class DataStatisticsService {
+    // 统计教学楼，类别，校区
+    @Autowired
+    private RoomMapper roomMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Map<String, Object> queryClassifyInfo() {
+        SelectStatementProvider teachBuildingState = selectDistinct(RoomDynamicSqlSupport.teachBuilding)
+                .from(RoomDynamicSqlSupport.room)
+                .groupBy(RoomDynamicSqlSupport.teachBuilding)
+                .build().render(RenderingStrategies.MYBATIS3);
+        SelectStatementProvider schoolState = selectDistinct(RoomDynamicSqlSupport.school)
+                .from(RoomDynamicSqlSupport.room)
+                .groupBy(RoomDynamicSqlSupport.school)
+                .build().render(RenderingStrategies.MYBATIS3);
+        SelectStatementProvider categoryState = selectDistinct(RoomDynamicSqlSupport.category)
+                .from(RoomDynamicSqlSupport.room)
+                .groupBy(RoomDynamicSqlSupport.category)
+                .build().render(RenderingStrategies.MYBATIS3);
+        List<String> teachBuildings = new ArrayList<>();
+        List<String> schools = new ArrayList<>();
+        List<String> categories =new ArrayList<>();
+        roomMapper.selectMany(teachBuildingState).forEach(v -> teachBuildings.add(v.getTeachBuilding()));
+        roomMapper.selectMany(schoolState).forEach(v -> schools.add(v.getSchool()));
+        roomMapper.selectMany(categoryState).forEach(v -> categories.add(v.getCategory()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("teachBuildings", teachBuildings);
+        map.put("schools", schools);
+        map.put("categories", categories);
+        return map;
+    }
+
+    // 获取用户学院信息
+    public Map<String, Object> queryUserInstitute() {
+        SelectStatementProvider statementProvider = selectDistinct(UserDynamicSqlSupport.institute)
+                .from(UserDynamicSqlSupport.user)
+                .groupBy(UserDynamicSqlSupport.institute)
+                .build().render(RenderingStrategies.MYBATIS3);
+        List<String> institutes = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        userMapper.selectMany(statementProvider).forEach(v -> institutes.add(v.getInstitute()));
+        map.put("institutes", institutes);
+        return map;
+    }
+
 }
