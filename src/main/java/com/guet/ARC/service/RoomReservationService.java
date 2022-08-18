30f206479cbf56aa39ba00ea7ca63c87df27f773
yulinfeng <2590213081@qq.com>
2022-08-18 15:57:37 +0800

    优化模块规划

--- src/main/java/com/guet/ARC/service/RoomReservationService.java
+++ src/main/java/com/guet/ARC/service/RoomReservationService.java
@@ -6,28 +6,36 @@ import com.guet.ARC.common.constant.CommonConstant;
 import com.guet.ARC.common.domain.PageInfo;
 import com.guet.ARC.common.domain.ResultCode;
 import com.guet.ARC.common.exception.AlertException;
-import com.guet.ARC.domain.Room;
 import com.guet.ARC.domain.RoomReservation;
 import com.guet.ARC.domain.User;
 import com.guet.ARC.domain.dto.apply.MyApplyQueryDTO;
-import com.guet.ARC.domain.dto.room.RoomQueryDTO;
+import com.guet.ARC.domain.dto.room.ApplyRoomDTO;
+import com.guet.ARC.domain.dto.room.RoomApplyDetailListQueryDTO;
+import com.guet.ARC.domain.vo.room.RoomReservationUserVo;
 import com.guet.ARC.mapper.RoomDynamicSqlSupport;
-import com.guet.ARC.mapper.RoomMapper;
 import com.guet.ARC.mapper.RoomReservationDynamicSqlSupport;
 import com.guet.ARC.mapper.RoomReservationMapper;
+import com.guet.ARC.mapper.UserMapper;
+import com.guet.ARC.util.CommonUtils;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.mybatis.dynamic.sql.SqlBuilder.*;
-import static org.mybatis.dynamic.sql.SqlBuilder.isEqualToWhenPresent;
 
 @Service
 public class RoomReservationService {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Autowired
     RoomReservationMapper roomReservationMapper;
 
@@ -45,6 +53,64 @@ public class RoomReservationService {
                 throw new AlertException(ResultCode.UPDATE_ERROR);
             }
         }
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public RoomReservation applyRoom(ApplyRoomDTO applyRoomDTO) {
+        String userId = StpUtil.getSessionByLoginId(StpUtil.getLoginId()).getString("userId");
+        long time = System.currentTimeMillis();
+        RoomReservation roomReservation = new RoomReservation();
+        roomReservation.setId(CommonUtils.generateUUID());
+        roomReservation.setRoomUsage(applyRoomDTO.getRoomUsage());
+        roomReservation.setReserveStartTime(applyRoomDTO.getStartTime());
+        roomReservation.setReserveEndTime(applyRoomDTO.getEndTime());
+        roomReservation.setState(CommonConstant.ROOM_RESERVE_TO_BE_REVIEWED);
+        roomReservation.setCreateTime(time);
+        roomReservation.setUpdateTime(time);
+        roomReservation.setUserId(userId);
+        roomReservation.setRoomId(applyRoomDTO.getRoomId());
+
+        if (roomReservationMapper.insert(roomReservation) == 0) {
+            throw new AlertException(ResultCode.INSERT_ERROR);
+        }
+        return roomReservation;
+    }
+
+    public PageInfo<RoomReservationUserVo> queryRoomApplyDetailList(RoomApplyDetailListQueryDTO roomApplyDetailListQueryDTO) {
+        // 查询相应房间的所有预约记录
+        SelectStatementProvider statementProvider = select(RoomReservationMapper.selectList)
+                .from(RoomReservationDynamicSqlSupport.roomReservation)
+                .where(RoomReservationDynamicSqlSupport.roomId, isEqualTo(roomApplyDetailListQueryDTO.getRoomId()))
+                .and(RoomReservationDynamicSqlSupport.createTime, isBetweenWhenPresent(roomApplyDetailListQueryDTO.getStartTime()).and(roomApplyDetailListQueryDTO.getEndTime()))
+                .orderBy(RoomReservationDynamicSqlSupport.createTime.descending())
+                .build().render(RenderingStrategies.MYBATIS3);
+
+        SelectStatementProvider statementProviderCount = select(count())
+                .from(RoomReservationDynamicSqlSupport.roomReservation)
+                .where(RoomReservationDynamicSqlSupport.roomId, isEqualTo(roomApplyDetailListQueryDTO.getRoomId()))
+                .and(RoomReservationDynamicSqlSupport.createTime, isBetweenWhenPresent(roomApplyDetailListQueryDTO.getStartTime()).and(roomApplyDetailListQueryDTO.getEndTime()))
+                .build().render(RenderingStrategies.MYBATIS3);
+
+        PageHelper.startPage(roomApplyDetailListQueryDTO.getPage(), roomApplyDetailListQueryDTO.getSize());
+        List<RoomReservation> roomReservationList = roomReservationMapper.selectMany(statementProvider);
+        List<RoomReservationUserVo> roomReservationUserVos = new ArrayList<>();
+        BeanCopier beanCopier = BeanCopier.create(RoomReservation.class, RoomReservationUserVo.class, false);
+        // 添加每条预约记录的预约人姓名
+        for (RoomReservation roomReservation : roomReservationList) {
+            RoomReservationUserVo roomReservationUserVo = new RoomReservationUserVo();
+            beanCopier.copy(roomReservation, roomReservationUserVo, null);
+            Optional<User> optionalUser = userMapper.selectByPrimaryKey(roomReservation.getUserId());
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                roomReservationUserVo.setUsername(user.getName());
+            }
+            roomReservationUserVos.add(roomReservationUserVo);
+        }
+        PageInfo<RoomReservationUserVo> pageInfo = new PageInfo<>();
+        pageInfo.setPage(roomApplyDetailListQueryDTO.getPage());
+        pageInfo.setTotalSize(roomReservationMapper.count(statementProviderCount));
+        pageInfo.setPageData(roomReservationUserVos);
+        return pageInfo;
     }
 
     public PageInfo<RoomReservation> queryMyApply(MyApplyQueryDTO myApplyQueryDTO) {
