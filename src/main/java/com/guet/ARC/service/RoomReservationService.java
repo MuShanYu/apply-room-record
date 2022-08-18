a09e5fe9445ed89ee2ae3f059bb90b8b557a4982
yulinfeng <2590213081@qq.com>
2022-08-18 17:14:50 +0800

    修复查询可预约房间列表和查询我的预约列表查询错误bug

--- src/main/java/com/guet/ARC/service/RoomReservationService.java
+++ src/main/java/com/guet/ARC/service/RoomReservationService.java
@@ -59,14 +59,16 @@ public class RoomReservationService {
     public RoomReservation applyRoom(ApplyRoomDTO applyRoomDTO) {
         // 检测是否已经预约
         String userId = StpUtil.getSessionByLoginId(StpUtil.getLoginId()).getString("userId");
-        // 是待审核状态且在这段预约时间内代表我已经预约过了
+        // 是待审核状态且在这段预约时间内代表我已经预约过了, 预约起始时间不能在准备预约的时间范围内，结束时间蹦年在准备结束预约的时间范围内
         SelectStatementProvider statementProvider = select(RoomReservationMapper.selectList)
                 .from(RoomReservationDynamicSqlSupport.roomReservation)
                 .where(RoomReservationDynamicSqlSupport.roomId, isEqualTo(applyRoomDTO.getRoomId()))
                 .and(RoomReservationDynamicSqlSupport.userId, isEqualTo(userId))
                 .and(RoomReservationDynamicSqlSupport.state, isEqualTo(CommonConstant.ROOM_RESERVE_TO_BE_REVIEWED))
-                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isLessThanOrEqualTo(applyRoomDTO.getStartTime()))
-                .and(RoomReservationDynamicSqlSupport.reserveEndTime, isGreaterThanOrEqualTo(applyRoomDTO.getEndTime()))
+                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isBetweenWhenPresent(applyRoomDTO.getStartTime())
+                        .and(applyRoomDTO.getEndTime()))
+                .or(RoomReservationDynamicSqlSupport.reserveEndTime, isBetweenWhenPresent(applyRoomDTO.getStartTime())
+                        .and(applyRoomDTO.getEndTime()))
                 .build().render(RenderingStrategies.MYBATIS3);
         List<RoomReservation> roomReservations = roomReservationMapper.selectMany(statementProvider);
         if (roomReservations.size() != 0) {
@@ -144,22 +146,28 @@ public class RoomReservationService {
                 .from(RoomReservationDynamicSqlSupport.roomReservation)
                 .leftJoin(RoomDynamicSqlSupport.room).on(RoomReservationDynamicSqlSupport.roomId, equalTo(RoomDynamicSqlSupport.id))
                 .where(RoomReservationDynamicSqlSupport.userId, isEqualTo(userId))
-                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime()).and(myApplyQueryDTO.getEndTime()))
                 .and(RoomReservationDynamicSqlSupport.state, isNotEqualTo(CommonConstant.STATE_NEGATIVE))
                 .and(RoomDynamicSqlSupport.school, isEqualToWhenPresent(myApplyQueryDTO.getSchool()))
                 .and(RoomDynamicSqlSupport.category, isEqualToWhenPresent(myApplyQueryDTO.getCategory()))
                 .and(RoomDynamicSqlSupport.teachBuilding, isEqualToWhenPresent(myApplyQueryDTO.getTeachBuilding()))
+                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime())
+                        .and(myApplyQueryDTO.getEndTime()))
+                .or(RoomReservationDynamicSqlSupport.reserveEndTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime())
+                        .and(myApplyQueryDTO.getEndTime()))
                 .build().render(RenderingStrategies.MYBATIS3);
 
         SelectStatementProvider statementProvider = select(RoomReservationMapper.selectList)
                 .from(RoomReservationDynamicSqlSupport.roomReservation)
                 .leftJoin(RoomDynamicSqlSupport.room).on(RoomReservationDynamicSqlSupport.roomId, equalTo(RoomDynamicSqlSupport.id))
                 .where(RoomReservationDynamicSqlSupport.userId, isEqualTo(userId))
-                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime()).and(myApplyQueryDTO.getEndTime()))
                 .and(RoomReservationDynamicSqlSupport.state, isNotEqualTo(CommonConstant.STATE_NEGATIVE))
                 .and(RoomDynamicSqlSupport.school, isEqualToWhenPresent(myApplyQueryDTO.getSchool()))
                 .and(RoomDynamicSqlSupport.category, isEqualToWhenPresent(myApplyQueryDTO.getCategory()))
                 .and(RoomDynamicSqlSupport.teachBuilding, isEqualToWhenPresent(myApplyQueryDTO.getTeachBuilding()))
+                .and(RoomReservationDynamicSqlSupport.reserveStartTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime())
+                        .and(myApplyQueryDTO.getEndTime()))
+                .or(RoomReservationDynamicSqlSupport.reserveEndTime, isBetweenWhenPresent(myApplyQueryDTO.getStartTime())
+                        .and(myApplyQueryDTO.getEndTime()))
                 .orderBy(RoomReservationDynamicSqlSupport.reserveStartTime.descending())
                 .build().render(RenderingStrategies.MYBATIS3);
 
