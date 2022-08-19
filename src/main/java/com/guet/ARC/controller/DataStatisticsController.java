cf7714430e6f52cce82eee2fe781e2b71b4e2e92
yulinfeng <2590213081@qq.com>
2022-08-19 18:31:08 +0800

    添加出入记录统计-待测试

--- src/main/java/com/guet/ARC/controller/DataStatisticsController.java
+++ src/main/java/com/guet/ARC/controller/DataStatisticsController.java
@@ -4,13 +4,17 @@ import cn.dev33.satoken.annotation.SaCheckRole;
 import cn.dev33.satoken.annotation.SaMode;
 import com.guet.ARC.common.anno.ResponseBodyResult;
 import com.guet.ARC.common.constant.CommonConstant;
+import com.guet.ARC.domain.Room;
 import com.guet.ARC.service.DataStatisticsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
 import java.util.Map;
 
 @Api(tags = "数据统计模块")
@@ -33,11 +37,26 @@ public class DataStatisticsController {
         return dataStatisticsService.queryUserInstitute();
     }
 
-    @GetMapping("/admin/get/roomReservationTimes")
+    @GetMapping("/admin/get/search/roomName")
+    @ApiOperation(value = "根据名称获取房间列表")
+    @SaCheckRole(value = {CommonConstant.ADMIN_ROLE, CommonConstant.SUPER_ADMIN_ROLE}, mode = SaMode.OR)
+    public List<Room> searchRoomByNameApi(@NotEmpty @RequestParam("roomName") String roomName) {
+        return dataStatisticsService.searchRoomByRoomName(roomName);
+    }
+
+    @GetMapping("/admin/get/roomReservationTimes/count")
     @ApiOperation(value = "获取会议室预约情况")
     @SaCheckRole(value = {CommonConstant.ADMIN_ROLE, CommonConstant.SUPER_ADMIN_ROLE}, mode = SaMode.OR)
-    public Map<String, Object> countRoomReservationTimes(String roomId, Long startTime) {
+    public Map<String, Object> countRoomReservationTimes(@RequestParam("roomId") String roomId,
+                                                         @RequestParam("startTime") Long startTime) {
         return dataStatisticsService.countRoomReservationTimes(roomId, startTime);
     }
 
+    @GetMapping("/admin/get/access/record/count")
+    @ApiOperation(value = "获取人员流动统计情况")
+    @SaCheckRole(value = {CommonConstant.ADMIN_ROLE, CommonConstant.SUPER_ADMIN_ROLE}, mode = SaMode.OR)
+    public Map<String, Object> countAccessRecordApi(@RequestParam("roomId") String roomId,
+                                                    @RequestParam("startTime") Long startTime) {
+        return dataStatisticsService.countAccessRecord(roomId, startTime);
+    }
 }
