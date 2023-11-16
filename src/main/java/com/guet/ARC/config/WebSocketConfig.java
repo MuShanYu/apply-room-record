177034cf7f3179f8cc2abc35a6fcedde8f26ba0d
yulf <yulf@startimes.com.cn>
2023-11-16 16:13:54 +0800

    添加消息socket基础服务

---
+++ src/main/java/com/guet/ARC/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.guet.ARC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Author: Yulf
+ * Date: 2023/11/16
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
