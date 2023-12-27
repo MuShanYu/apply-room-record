be4d5ee13c34113a18c14546a502f59ab6220612
yulf <yulf@startimes.com.cn>
2023-12-27 17:24:46 +0800

    待审核房价列表添加查询条件时间区间

--- src/main/java/com/guet/ARC/domain/dto/room/RoomReserveReviewedDTO.java
+++ src/main/java/com/guet/ARC/domain/dto/room/RoomReserveReviewedDTO.java
@@ -1,6 +1,8 @@
 package com.guet.ARC.domain.dto.room;
 
 import com.guet.ARC.domain.enums.ReservationState;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 import org.hibernate.validator.constraints.Range;
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @ToString
+@ApiModel(value = "RoomReserveReviewedDTO", description = "待审核信息查询DTO")
 public class RoomReserveReviewedDTO {
     @Min(1)
     private Integer page;
@@ -17,16 +20,28 @@ public class RoomReserveReviewedDTO {
     @Range(min = 1, max = 100)
     private Integer size;
 
+    @ApiModelProperty(value = "查询校区")
     private String school;
 
+    @ApiModelProperty(value = "查询楼栋")
     private String teachBuilding;
 
+    @ApiModelProperty(value = "查询房价类型")
     private String category;
 
-    @NotNull(message = "状态不能为空")
+    @ApiModelProperty(value = "审核状态")
+    @NotNull(message = "状态不能为")
     private Integer state;
 
+    @ApiModelProperty(value = "查询学号")
     private String stuNum;
 
+    @ApiModelProperty(value = "后端自动设置")
     private String applyUserId;
+
+    @ApiModelProperty(value = "查询时间区间-起始时间戳，毫秒数")
+    private Long startTime;
+
+    @ApiModelProperty(value = "查询时间区间-结束时间戳，毫秒数")
+    private Long endTime;
 }
