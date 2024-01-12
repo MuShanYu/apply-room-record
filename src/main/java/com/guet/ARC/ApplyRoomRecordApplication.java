1d99d87b7163c9bb43ae39ec3a23f7b9f3a32bcc
yulf <yulf@startimes.com.cn>
2024-01-12 16:24:44 +0800

    添加远程数据库，添加注册功能验证码

--- src/main/java/com/guet/ARC/ApplyRoomRecordApplication.java
+++ src/main/java/com/guet/ARC/ApplyRoomRecordApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableAsync
 public class ApplyRoomRecordApplication {
 
     public static void main(String[] args) {
