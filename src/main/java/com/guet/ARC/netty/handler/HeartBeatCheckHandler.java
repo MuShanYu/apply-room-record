aea8a9c84bef656e079df9e342b43a35e94f7be6
yulf <yulf@startimes.com.cn>
2024-12-06 14:15:15 +0800

    promotion：添加服务端心跳检测机制、ssl加密数据校验（防止异常流量），现在双端心跳检测，避免异常关闭后通道未关闭，引发Connection reset异常。

--- src/main/java/com/guet/ARC/netty/handler/HeartBeatCheckHandler.java
+++ src/main/java/com/guet/ARC/netty/handler/HeartBeatCheckHandler.java
@@ -1,5 +1,6 @@
 package com.guet.ARC.netty.handler;
 
+import com.guet.ARC.netty.manager.UserOnlineManager;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -37,6 +38,8 @@ public class HeartBeatCheckHandler extends SimpleChannelInboundHandler<TextWebSo
             if (state == IdleState.READER_IDLE) {
                 // 服务端10s没有收到客户端的心跳消息了，或者其他消息（用户发送的信息），服务端主动断开连接
                 log.warn("客户端异常退出，10s未收到心跳消息，服务端主动断开连接。userId: {}", ctx.channel().attr(AttributeKey.valueOf("userId")).get());
+                // 可能连接过了
+                UserOnlineManager.removeChannel(ctx.channel());
                 ctx.close();
             }
         }
